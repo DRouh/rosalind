c522e95edf622fc3bc542ea8c184cacb56793d26
DRouh <dimerouh@gmail.com>
2017-08-27 17:29:22 +0300

    F#: solve `Consensus and Profile`

---
+++ fs/Consensus and Profile/cons.fsx
@@ -0,0 +1,47 @@
+let readFasta (x:string) =
+  x.Split([|"\n"; "\r\n"|], System.StringSplitOptions.RemoveEmptyEntries)
+  |> Seq.fold (fun acc (x:string) ->
+                   match (acc, x) with
+                   | (xs, y)  when y.StartsWith(">") -> (y.Substring(1), "") :: xs
+                   | ((r,rs)::xs, y) -> (r, rs + y) :: xs
+                   | otherwise -> []) []
+
+let toMatrix = List.map(List.ofSeq)
+
+let rec transposeMatrix (x:'a list list) :'a list list = 
+  match x |> List.filter (List.isEmpty >> not) with
+  | [] -> []
+  | xs -> (List.map List.head xs) :: transposeMatrix (List.map List.tail xs)
+
+let sum = 
+  let plus (a, b, c, d) (x, y, z, w) = 
+    (a + x, b + y, c + z, d + w)
+  let tuplify = function 
+                | 'A' -> (1, 0, 0, 0)
+                | 'C' -> (0, 1, 0, 0) 
+                | 'G' -> (0, 0, 1, 0) 
+                | 'T' -> (0, 0, 0, 1)
+  let tupleToList = function (a, c, g, t) -> [a; c; g; t]   
+
+  List.map tuplify
+  >> List.fold plus (0, 0, 0, 0)
+  >> tupleToList
+
+let consensus = 
+  let byIndex i = [|'A'; 'C'; 'G'; 'T'|].[i]
+
+  List.map (List.indexed >> List.maxBy snd >> fst >> byIndex)
+  >> System.String.Concat
+
+let input = System.IO.File.ReadAllText("rosalind_gc.txt")
+let fasta = input 
+          |> readFasta 
+          |> List.sortBy (fst) 
+          |> List.map snd
+let matrix = fasta 
+           |> toMatrix 
+           |> transposeMatrix
+           |> List.map sum
+let con = consensus matrix
+
+System.IO.File.WriteAllText("output.txt", con)
